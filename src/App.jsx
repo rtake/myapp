aa61c32251d632cea023b9243eb749f42cf5572d
rtake <rtake1110@gmail.com>
2023-07-02 20:11:45 +0900

    ボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 export const App = () => {
+    const onClickButton = () => {
+        alert();
+    };
+
     return (
         <div>
             <hl>こんにちは！</hl>
             <p>お元気ですか？</p>
+            <button onClick={onClickButton}>ボタン</button>
         </div>
     );
 }
