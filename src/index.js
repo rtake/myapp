9c24325c4f0c3183b7070f8b1d87a83873a2ddbc
rtake <rtake1110@gmail.com>
2023-07-02 20:00:57 +0900

    index.js を新規作成

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <hl>こんにちは！</hl>
+            <p>お元気ですか？</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
