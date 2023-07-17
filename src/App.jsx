c73e57dde43e90aab05d162a1152f8eaa607fcdc
rtake <rtake1110@gmail.com>
2023-07-17 17:56:28 +0900

    名前編集アプリを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { useState, memo, useCallback } from "react";
-import { Child1 } from "./components/Child1";
-import { Child4 } from "./components/Child4";
+import { useState } from "react";
+import { Card } from "./components/Card"
 
-export const App = memo(() => {
-    const [num, setNum] = useState(0);
-
-    const onClickButton = () => {
-        setNum(num+1);
+export const App = () => {
+    const [ isAdmin, setIsAdmin ] = useState(false);
+    const onClickSwitch = () => { 
+        setIsAdmin(!isAdmin);
     };
-
-    // 実行されるたびに生成されるので,再レンダリングが起きる
-    // useCallback を使い, 再生成されないようにする
-    const onClickReset = useCallback(() => {
-        setNum(0);
-    }, []);
 
     return (
         <div>
-            <button onClick={onClickButton}>ボタン</button>
-            <p>{num}</p>
-            <Child1 onClickReset={onClickReset} />
-            <Child4/>
+            {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
+            <button onClick={onClickSwitch}>切り替え</button>
+            <Card isAdmin={isAdmin} />
         </div>
     );
-});
+};
 
 export default App;
